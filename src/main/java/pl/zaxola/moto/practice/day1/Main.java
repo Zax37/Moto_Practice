234b947c5ac1e3c79d7c804d322337a1d4f6798e
Zax37 <c37zax@gmail.com>
2018-02-17 10:08:48 +0100

    Initial commit with gradle project structure.

---
+++ src/main/java/pl/zaxola/moto/practice/day1/Main.java
@@ -0,0 +1,8 @@
+package pl.zaxola.moto.practice.day1;
+
+public class Main {
+    public static void main(String[] args) {
+        Library classUnderMain = new Library();
+        System.out.println(classUnderMain.someLibraryMethod());
+    }
+}
